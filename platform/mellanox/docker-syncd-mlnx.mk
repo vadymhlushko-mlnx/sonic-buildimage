523f42982a1d6c8b17f07631584e89b030d5681d
vadymhlushko-mlnx <vadymh@nvidia.com>
2023-07-13 18:49:21 +0300

    Add new path /etc/sonic/mft/fwtrace_cfg
    Signed-off-by: vadymhlushko-mlnx <vadymh@nvidia.com>

--- platform/mellanox/docker-syncd-mlnx.mk
+++ platform/mellanox/docker-syncd-mlnx.mk
@@ -40,4 +40,4 @@ $(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
-$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /etc/mft:/etc/mft
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /etc/sonic/mft:/etc/sonic/mft:ro
